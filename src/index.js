ce2a11c3c3843b8fab9708edc28b9d8a6baad0be
MaksymSkala <maksymskala@gmail.com>
2023-08-31 21:47:42 +0300

    add

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ async function displayCatInfo(breedId) {
   try {
     const cat = await fetchCatByBreed(breedId);
     catInfo.innerHTML = `
-      <img src="${cat.url}" alt="${cat.breeds[0].name}" width="200px" height="200px" />
+      <img src="${cat.url}" alt="${cat.breeds[0].name}" width="500px" height="500px" />
       <h2>${cat.breeds[0].name}</h2>
       <p>${cat.breeds[0].description}</p>
       <p>Temperament: ${cat.breeds[0].temperament}</p>
