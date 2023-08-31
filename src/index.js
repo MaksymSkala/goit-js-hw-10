87bbe0d4999e1d94e1e38f6b528bb05bf7f6c7b0
MaksymSkala <maksymskala@gmail.com>
2023-08-31 14:02:47 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
-const breedSelect = document.querySelector(".breed-select");
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-const catInfo = document.querySelector(".cat-info");
+const catImage = document.getElementById("cat-image");
+const catDescription = document.getElementById("cat-description");
 
-async function populateBreedSelect() {
+async function displayCatInfo(breedId) {
   try {
-    const breeds = await fetchBreeds();
-    breeds.forEach(breed => {
-      const option = document.createElement("option");
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
-    });
+    const cat = await fetchCatByBreed(breedId);
+    catImage.src = cat.url; // Встановлюємо значення атрибуту src
+    catImage.alt = cat.breeds[0].name;
+    catDescription.innerHTML = `
+      <h2>${cat.breeds[0].name}</h2>
+      <p>${cat.breeds[0].description}</p>
+      <p>Temperament: ${cat.breeds[0].temperament}</p>
+    `;
   } catch (err) {
-    console.error("Error fetching breeds:", err);
-    error.textContent = "Oops! Something went wrong while fetching breeds.";
-  } finally {
-    loader.style.display = "none";
+    console.error("Error fetching cat information:", err);
+    // Handle error
   }
 }
 
